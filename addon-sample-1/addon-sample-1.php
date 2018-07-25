4e1addc9270dd8ac2a118395762f941cd5783907
Varun Sridharan <varunsridharan23@gmail.com>
2018-07-25 16:25:33 +0530

    Create addon-sample-1.php

---
+++ addon-sample-1/addon-sample-1.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Addon Name:Addon Sample 1
+ * Addon URI:https://codecanyon.net/item/booked-payments-with-woocommerce-addon/12824257?ref=varunsridharan
+ * Version:1.0
+ * Description:Integrates With Booked Payments with WooCommerce (Add-On) which allows you to sell appoinments with role based pricing
+ * Author:Varun Sridharan
+ * Author URI:http://varunsridharan.in
+ * Last Update:2018-02-27
+ * Category:General,Integration
+ * [REQUIRED_PLUGINS]
+ *  [woocommerce-role-based-price/woocommerce-role-based-price.php]
+ *     Name:WC Role Based Price
+ *     Author:Varun Sridharan
+ *     Required Version:1.0
+ *     URL:https://codecanyon.com/item/booked-appointments-appointment-booking-for-wordpress/9466968?ref=varunsridharan
+ *  [/woocommerce-role-based-price/woocommerce-role-based-price.php]
+ * [/REQUIRED_PLUGINS]
+ * [SCREENSHOTS]
+ *     [https://s3.envato.com/files/236208489/nab-transact-directpost-checkout.png] Simple Thing To Do
+ *     [http://s.svarun.in/cD]
+ *     [screenshot-1.png] I  know i like it
+ * [/SCREENSHOTS]
+ */
