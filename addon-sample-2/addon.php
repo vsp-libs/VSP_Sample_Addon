f15272290d73764a9dc29f8c1e50c5b1affaa659
Varun Sridharan <varunsridharan23@gmail.com>
2018-07-25 16:25:58 +0530

    Create addon.php

---
+++ addon-sample-2/addon.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Addon Name: Addon Sample 2
+ * Addon URI: https://codecanyon.net/item/booked-payments-with-woocommerce-addon/12824257?ref=varunsridharan
+ * Version: 1.0
+ * Description: Integrates With Booked Payments with WooCommerce (Add-On) which allows you to sell appoinments with role based pricing 
+ * Author: Varun Sridharan
+ * Author URI: http://varunsridharan.in
+ * Last Update: 2016-03-04 
+ * Required Plugins: [ Name : Booked | URL : https://codecanyon.net/item/booked-appointments-appointment-booking-for-wordpress/9466968?ref=varunsridharan | Version : 1.9.10 or higher | Slug : booked/booked.php] , [ Name : Booked Payments with WooCommerce | URL : https://codecanyon.net/item/booked-payments-with-woocommerce-addon/12824257?ref=varunsridharan | Version : 1.3.3 or higher | Slug : booked-woocommerce-payments/booked-woocommerce-payments.php]
+ * Category:Integration,DID,JK
+ */
